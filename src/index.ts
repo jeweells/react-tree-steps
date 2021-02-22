0eb9430ae175df58bf14adfe3c6d19ab93846cf3
jeweells <37516710+jeweells@users.noreply.github.com>
2021-02-22 16:12:42 -0400

    Small fix

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@
 export {TreeSteps} from "./TreeSteps";
 export {node, tunnelNode} from "./TreeSteps/utils";
 export {TreeContext} from "./TreeSteps/context";
-export {Transition} from "./TreeSteps/Transition";
